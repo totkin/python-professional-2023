9af70ff8a88b542fcceac6a95d241d6822f7e567
Alexander <134145027+totkin@users.noreply.github.com>
2023-12-24 11:48:49 +0300

    Add homework 3

--- homework_01/log-analyzer/log_analyzer.py
+++ homework_01/log-analyzer/log_analyzer.py
@@ -27,8 +27,52 @@ config_local = {
     "APP_DEBUG_LOG_FILE_PATH": "./resources/debug-log/debug_log.log",
     "APP_NAME": os.path.basename(sys.argv[0]).replace('.py', ''),
     "APP_CONFIG_DEFAULT_PATH": "./resources/config.json",
-    "APP_VERSION": "0.1.20231216",
+    "APP_VERSION": "0.1.20231220",
+    "APP_FILE_LAST_EFFECTIVE_START": "./resources/last_effective_start.json",
 }
+
+
+class WorkFile:
+    """
+    # {"обрабатываем":"", "имя файла":"", "время прошлой обработки":"", "результат прошлой обработки":""}
+    """
+    _file_name = config_local.get("APP_FILE_LAST_EFFECTIVE_START")
+
+    def __init__(self):
+        if os.path.exists(self._file_name):
+
+            self.PROCESSING: bool = True
+            self.FILE_NAME: str = "nginx-access-ui.log-20170630.gz"
+            self.LAST_PROCESSING_DATETIME: str = "2023.12.2012:48:16"
+            self.LAST_PROCESSING_RESULT: str = "OK"
+            self.LAST_PROCESSING_QUALITY: float = 1.0
+        else:
+            self.PROCESSING: bool = True
+            self.FILE_NAME: str = "nginx-access-ui.log-20170630.gz"
+            self.LAST_PROCESSING_DATETIME: str = "2023.12.2012:48:16"
+            self.LAST_PROCESSING_RESULT: str = "OK"
+            self.LAST_PROCESSING_QUALITY: float = 1.0
+
+    def __str__(self, ):
+        return json.dumps(self, default=lambda o: o.__dict__)
+
+    def __repr__(self, ):
+        return self.__str__()
+
+    def find(self, logs_path: str):
+        """
+        # проверить существование пути расположения логов
+        # найти самый новый файл
+        # проверить, что он не обрабатывался, если уже обрабатывался, то вернуть результат (успех-падение, % охвата)
+        # return work_file
+        """
+        return
+
+    def save(self, ):
+        json_object = json.dumps(self, default=lambda o: o.__dict__)
+        with open(self._file_name, 'w') as f:
+            f.write(json_object)
+        return
 
 
 def first_description_print() -> None:
@@ -72,9 +116,12 @@ def create_parser() -> ArgumentParser:
     return parser
 
 
-def log_init():
-
-    # Нормальный режим
+def log_init() -> None:
+    """
+    Инициализация logging - для DEBUG и NORMAL режимов работы
+    :return: None
+    """
+    # NORMAL mode
     if not config_local.get("APP_DEBUG"):
         loc_file_path = config.get('APP_LOG_FILE_PATH')
         loc_str = '[%(asctime)s] %(levelname).1s %(message)s'
@@ -85,9 +132,13 @@ def log_init():
             datefmt='%Y.%m.%d%H:%M:%S'
         )
 
-    # Режим отладки
+    # DEBUG mode
     else:
         loc_file_path = config_local.get('APP_DEBUG_LOG_FILE_PATH')
+        loc_dir_path = os.path.dirname(loc_file_path)
+        if not os.path.isdir(loc_dir_path):
+            os.makedirs(loc_dir_path)
+
         loc_str = "%(asctime)s ; [%(levelname)s] ; %(funcName)s ; %(lineno)d ; %(message)s"
         logging.basicConfig(
             filename=loc_file_path,
@@ -102,14 +153,53 @@ def log_init():
 
 
 def get_config(config_path) -> typing.Dict:
-    with open(config_path, 'r', encoding='UTF-8') as loc_file:
-        config_extra = json.load(loc_file)
+    result = {**config}
+    if os.path.exists(config_path):
+        with open(config_path, 'r', encoding='UTF-8') as loc_file:
+            config_extra = json.load(loc_file)
+        result = {**config, **config_extra}
+    else:
+        if config_local.get("APP_DEBUG"):
+            logging.warning("config file not exist")
 
-    result = {**config, **config_extra}
+    loc_file_name = config_local.get("APP_FILE_LAST_EFFECTIVE_START")
+    with open(loc_file_name, 'r', encoding='UTF-8') as loc_file:
+        WorkFile = json.load(loc_file)
 
-    logging.info(result)
+    if config_local.get("APP_DEBUG"):
+        logging.info(result)
 
     return result
+
+
+def get_latest_log(logs_path: str) -> typing.Dict:
+    # проверить существование пути расположения логов
+    # найти самый новый файл
+    # проверить, что он не обрабатывался, если уже обрабатывался, то вернуть результат (успех-падение, % охвата)
+    # return work_file
+
+    if not os.path.exists(logs_path):
+        return
+
+    file_name_template = re.compile(r"""nginx-access-ui\.log-(?P<log_date>\d{8})(\.gz$|$)""", re.IGNORECASE)
+
+    latest_log = None
+    for path in os.listdir(logs_path):
+        file_log = re.search(file_name_template, path)
+
+        if not file_log or os.path.isdir(path):
+            continue
+
+        file_name_data = file_log.groupdict()
+        try:
+            file_date = datetime.strptime(file_name_data['log_date'], '%Y%m%d').date()
+        except ValueError:
+            continue
+
+        if not latest_log or latest_log.file_date < file_date:
+            latest_log = LogFile(path=os.path.join(logs_path, path), file_date=file_date)
+
+    return latest_log
 
 
 def main():
@@ -121,6 +211,10 @@ if __name__ == "__main__":
     namespace = parser.parse_args(sys.argv[1:])
 
     first_description_print()
+
+    wf = WorkFile()
+    print(wf)
+    wf.save()
 
     log_init()
     current_config: typing.Dict = get_config(namespace.config.name)
